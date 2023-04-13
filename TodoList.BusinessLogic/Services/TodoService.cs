f6db304a8372c6c887a39db30334c74d11e4ec83
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-04-13 18:56:29 +0000

    Add CRUD task

---
+++ TodoList.BusinessLogic/Services/TodoService.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.BusinessLogic.Interfaces;
+using TodoList.BusinessLogic.Models;
+using TodoList.DataAccess.Data;
+using TodoList.DataAccess.Models;
+
+namespace TodoList.BusinessLogic.Services
+{
+    public class TodoService : ITodo
+    {
+        public readonly TodoApiDbContext _DbContext;
+
+        public TodoService(TodoApiDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public async Task<Todo> AddTodoAsync(TodoRequest todo)
+        {
+            Todo todoAdd = new Todo()
+            {
+                Title = todo.Title,
+                Description = todo.Description,
+                Status = todo.Status,
+                CreatedAt = DateTime.Now,
+            };
+
+            await _DbContext.Todos.AddAsync(todoAdd);
+            await _DbContext.SaveChangesAsync();
+
+            return todoAdd;
+        }
+
+        public async Task<Todo> GetTodoAsync(Guid id)
+        {
+            var todo = await _DbContext.Todos.FindAsync(id);
+
+            if (todo == null)
+            {
+                return null;
+            }
+
+            return todo;
+        }
+
+        public IEnumerable<Todo> GetTodos()
+        {
+            return _DbContext.Todos.ToList();
+        }
+
+        public async Task<bool> RemoveTodoAsync(Guid id)
+        {
+            var todo = await _DbContext.Todos.FindAsync(id);
+
+            if (todo == null)
+            {
+                return false;
+            }
+
+            _DbContext.Remove(todo);
+
+            await _DbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Todo> UpdateTodoAsync(Guid id, TodoRequest todoRequest)
+        {
+            var todo = await _DbContext.Todos.FindAsync(id);
+
+            if(todo == null)
+            {
+                return null;
+            }
+
+            todo.Status = todoRequest.Status;
+            todo.Title = todoRequest.Title;
+            todo.Description = todoRequest.Description;
+            todo.UpdatedAt = DateTime.Now;
+
+            await _DbContext.SaveChangesAsync();
+
+            return todo;
+        }
+    }
+}
---
+++ TodoList.DataAccess/Migrations/20230413182349_testj.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.DataAccess.Migrations
+{
+    public partial class testj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
